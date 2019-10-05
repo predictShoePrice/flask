aeb9e4bd6c5ce5c0ef9dec3239c40c5daa6057dc
Narsiss <vvwyot@126.com>
2019-10-05 17:52:09 +0800

    优化请求

--- web/controllers/api/ShoesDetail.py
+++ web/controllers/api/ShoesDetail.py
@@ -9,14 +9,25 @@ from application import app, db
 import json
 import pandas as pd
 import numpy as np
+import datetime
 
 
-@route_api.route("/item/detail", methods=["post"])
+@route_api.route("/item/detail", methods=["get"])
 def Detail():
-    resp = {'code': 200, 'msg': '操作成功~', 'items': {}}
-    req = request.values
+    values = request.values
+    sku_id = values['sku_id']
+    add_time = values['date']
+    if not sku_id:
+        resp = {'code': 500, 'msg': '参数缺失: sku_id'}
+        return jsonify(resp)
 
-    query_detail = ShoesDetail.query.filter_by(sku_id=req['sku_id']).first()
+    if not add_time:
+        resp = {'code': 500, 'msg': '参数缺失: add_time'}
+        return jsonify(resp)
+
+    resp = {'code': 200, 'msg': '操作成功~', 'data': {}}
+
+    query_detail = ShoesDetail.query.filter_by(sku_id=sku_id).first()
     item_dict = {}
     item_dict['product_id'] = query_detail.product_id
     item_dict['sku_id'] = query_detail.sku_id
@@ -29,34 +40,64 @@ def Detail():
     item_dict['image_path'] = query_detail.image_path
     item_dict['sale_date'] = query_detail.sale_date
     item_dict['hot_product'] = query_detail.hot_product
-    resp['items'] = item_dict
 
-    add_time = ShoesPlatform.query.with_entities(ShoesPlatform.add_time).distinct().all()
-    add_time = sorted([int(x[0]) for x in add_time])[-1]
-    query_platform = ShoesPlatform.query.filter(
-        and_(ShoesPlatform.sku_id == req['sku_id'], ShoesPlatform.add_time == add_time)).all()
-    platforms = ShoesPlatform.query.with_entities(ShoesPlatform.platform).distinct().all()
-    platforms = [x[0] for x in platforms]
-    shoe_size = ShoesPlatform.query.with_entities(ShoesPlatform.shoe_size).distinct().all()
-    shoe_size = sorted([float(x[0]) for x in shoe_size])
-    df = pd.DataFrame(index=platforms, columns=shoe_size)
-    for item in query_platform:
-        df.at[item.platform, float(item.shoe_size)] = [item.purchase_price, item.platform_price]
-    df = np.array(df)
-    size_list = []
-    for _i in range(3):
-        size_list.append({platforms[_i]: df.tolist()[_i]})
-    resp['size'] = size_list
-    resp['shoe_size'] = shoe_size
+    resp['data']['detail'] = item_dict
+
+    # today = datetime.datetime.now()
+    # startTime = today - datetime.timedelta(days=7)
+    # resp['data']['startTime'] = startTime.strftime("%Y%m%d")
+
+    platforms = ['nice', 'stockx', 'du']
+    resp['data']['platforms'] = platforms
+
+    quotes_set = {}
+    platform_data = ShoesPlatform.query.filter_by(sku_id=sku_id, add_time=add_time)
+    for item in platform_data:
+        shoe_size_index = 'index_' + item.shoe_size
+        if shoe_size_index in quotes_set:
+            quotes = quotes_set[shoe_size_index]
+        else:
+            quotes = {
+                'time': item.add_time,
+                'size': item.shoe_size,
+                'ask_price': [0,0,0],
+                'bid_price': [0,0,0]
+            }
+            
+        platform_index = platforms.index(item.platform)
+        if platform_index<0: 
+            continue
+        quotes['ask_price'][platform_index] = item.purchase_price
+        quotes['bid_price'][platform_index] = item.platform_price
+        quotes_set[shoe_size_index] = quotes
+    resp_quotes = []
+    for key in sorted(quotes_set.keys()):
+        resp_quotes.append(quotes_set[key])
+    resp['data']['quotes'] = resp_quotes
     return jsonify(resp)
 
 
-@route_api.route("/item/trend", methods=["post"])
+@route_api.route("/item/trend", methods=["get"])
 def ItemTrend():
-    resp = {'code': 200, 'msg': '操作成功~', 'items': {}}
-    req = request.values
+    values = request.values
+    sku_id = values['sku_id']
+    size = values['size']
+    start_time = values['start_time']
+    if not sku_id:
+        resp = {'code': 500, 'msg': '参数缺失: sku_id'}
+        return jsonify(resp)
 
-    query_detail = ShoesDetail.query.filter_by(sku_id=req['sku_id']).first()
+    if not start_time:
+        resp = {'code': 500, 'msg': '参数缺失: start_time (YYYYMMDD)'}
+        return jsonify(resp)
+
+    if not size:
+        resp = {'code': 500, 'msg': '参数缺失: size'}
+        return jsonify(resp)
+
+    resp = {'code': 200, 'msg': '操作成功~', 'data': {}}
+
+    query_detail = ShoesDetail.query.filter_by(sku_id=sku_id).first()
     item_dict = {}
     item_dict['product_id'] = query_detail.product_id
     item_dict['sku_id'] = query_detail.sku_id
@@ -69,23 +110,38 @@ def ItemTrend():
     item_dict['image_path'] = query_detail.image_path
     item_dict['sale_date'] = query_detail.sale_date
     item_dict['hot_product'] = query_detail.hot_product
-    resp['items'] = item_dict
 
-    add_time = ShoesPlatform.query.with_entities(ShoesPlatform.add_time).distinct().all()
-    add_time = sorted([int(x[0]) for x in add_time])[-1]
-    query_platform = ShoesPlatform.query.filter(
-        and_(ShoesPlatform.sku_id == req['sku_id'], ShoesPlatform.add_time == add_time)).all()
-    platforms = ShoesPlatform.query.with_entities(ShoesPlatform.platform).distinct().all()
-    platforms = [x[0] for x in platforms]
-    shoe_size = ShoesPlatform.query.with_entities(ShoesPlatform.shoe_size).distinct().all()
-    shoe_size = sorted([float(x[0]) for x in shoe_size])
-    df = pd.DataFrame(index=platforms, columns=shoe_size)
-    for item in query_platform:
-        df.at[item.platform, float(item.shoe_size)] = [item.purchase_price, item.platform_price]
-    df = np.array(df)
-    size_list = []
-    for _i in range(3):
-        size_list.append({platforms[_i]: df.tolist()[_i]})
-    resp['size'] = size_list
-    resp['shoe_size'] = shoe_size
+    resp['data']['detail'] = item_dict
+
+    # today = datetime.datetime.now()
+    # startTime = today - datetime.timedelta(days=7)
+    # resp['data']['startTime'] = startTime.strftime("%Y%m%d")
+
+    platforms = ['nice', 'stockx', 'du']
+    resp['data']['platforms'] = platforms
+
+    quotes_set = {}
+    platform_data = ShoesPlatform.query.filter_by(sku_id=sku_id, shoe_size=size).filter(ShoesPlatform.add_time>=start_time)
+    for item in platform_data:
+        time_index = 'index_' + item.add_time
+        if time_index in quotes_set:
+            quotes = quotes_set[time_index]
+        else:
+            quotes = {
+                'time': item.add_time,
+                'size': item.shoe_size,
+                'ask_price': [0,0,0],
+                'bid_price': [0,0,0]
+            }
+            
+        platform_index = platforms.index(item.platform)
+        if platform_index<0: 
+            continue
+        quotes['ask_price'][platform_index] = item.purchase_price
+        quotes['bid_price'][platform_index] = item.platform_price
+        quotes_set[time_index] = quotes
+    resp_quotes = []
+    for key in sorted(quotes_set.keys()):
+        resp_quotes.append(quotes_set[key])
+    resp['data']['quotes'] = resp_quotes
     return jsonify(resp)
