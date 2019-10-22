88f2767537e870100b36ebb6594e71f81777f885
Narsiss <vvwyot@126.com>
2019-10-22 11:19:16 +0800

    Quotes Job

---
+++ common/models/shoes/ShoesQuotes.py
@@ -0,0 +1,24 @@
+# coding: utf-8
+from sqlalchemy import Column, DateTime, Index, String, text, Float
+from sqlalchemy.ext.declarative import declarative_base
+from application import db, app
+
+
+class ShoesQuotes(db.Model):
+    __bind_key__ = 'shoes'
+    __tablename__ = 'shoes_quotes_day'
+    __table_args__ = (
+        Index('sku_key', unique=True),
+    )
+
+    sku_key = Column(String(255), primary_key=True)
+    sku = Column(String(255))
+    size = Column(String(255))
+    time_str = Column(String(255))
+    time = Column(DateTime)
+    pre_close = Column(Float)
+    open = Column(Float)
+    high = Column(Float)
+    low = Column(Float)
+    close = Column(Float)
+    volume = Column(Float)
---
+++ jobs/tasks/quotes/index.py
@@ -0,0 +1,96 @@
+# -*- coding: utf-8 -*-
+from application import app,db
+from common.models.shoes.ShoesQuotes import ShoesQuotes
+from common.models.shoes.ShoesDetail import ShoesDetail
+from common.models.shoes.ShoesPlatform import ShoesPlatform
+import json,requests,datetime
+from common.libs.Helper import getCurrentDate
+from sqlalchemy import func
+
+
+'''
+python manager.py runjob -m quotes/index
+'''
+
+class JobTask():
+    def __init__(self):
+        pass
+    def run(self,params):
+        list = ShoesDetail.query.all()
+        today = getCurrentDate()
+        for day in range(0,30):
+            for item in list:
+                self.handleItem(item, today - datetime.timedelta(day))
+
+    def handleItem(self,item, today):
+        yesterday = today + datetime.timedelta(-1)
+        today_str = getCurrentDate().strftime('%Y%m%d')
+        yesterday_str = yesterday.strftime('%Y%m%d')
+        sku = item.sku_id
+        
+        quotes_set = {}
+
+        # 查询今天的价格
+        today_platform_details = ShoesPlatform.query.filter_by(sku_id=sku, add_time=today_str)
+        for platform in today_platform_details:
+            sku_key = yesterday_str + '_' + sku + '_' + platform.shoe_size
+            quotes = None
+            if sku_key in quotes_set:
+                quotes = quotes_set.get(sku_key)
+            else:
+                quotes = ShoesQuotes()
+                quotes.sku_key = sku_key
+                quotes.size = platform.shoe_size
+                quotes.sku = sku
+                quotes.time = today
+                quotes.time_str = today_str
+                quotes.pre_close = 0.0
+                price = 0
+                if platform.platform_price != '-':
+                    price = float(platform.platform_price)
+                quotes.open = price
+                quotes.high = price
+                quotes.low = price
+                quotes.close = price
+                quotes.volume = 0
+
+            price = quotes.open
+            compare_price = 0
+
+            if platform.platform_price != '-':
+                compare_price = float(platform.platform_price)
+            if compare_price is 0: 
+                continue
+            if price<= 0.0 or price>compare_price:
+                quotes.open = compare_price
+                quotes.high = compare_price
+                quotes.low = compare_price
+                quotes.close = compare_price
+            quotes_set[sku_key] = quotes
+
+        # 查询昨天的价格
+        yesterday_platform_details = ShoesPlatform.query.filter_by(sku_id=sku, add_time=yesterday_str)
+        for platform in yesterday_platform_details:
+            app.logger.info(platform)
+            sku_key = yesterday_str + '_' + sku + '_' + platform.shoe_size
+            quotes = {}
+            if sku_key in quotes_set:
+                quotes = quotes_set.get(sku_key)
+            else:
+                # 今日无行情的直接跳过
+                continue
+
+            price = quotes.pre_close
+            compare_price = 0
+            if platform.platform_price != '-':
+                compare_price = float(platform.platform_price)
+            if compare_price is 0: 
+                continue
+            if price<=0.0 or price>compare_price:
+                quotes.pre_close = compare_price
+            quotes_set[sku_key] = quotes
+
+        for quotes in quotes_set.values():
+            db.session.add(quotes)
+            db.session.commit()
+        return True
