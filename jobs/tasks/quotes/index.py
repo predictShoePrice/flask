b0c57d057d4dae2ba7f1388df61dde2804b1eb34
Narsiss <vvwyot@126.com>
2019-10-29 23:42:26 +0800

    qutoes

--- jobs/tasks/quotes/index.py
+++ jobs/tasks/quotes/index.py
@@ -20,8 +20,7 @@ class JobTask():
         list = ShoesDetail.query.all()
         today = getCurrentDate()
         for day in range(1, 3):
-            for item in list:
-                self.handleItem(item, today - datetime.timedelta(3-day))
+            self.handleItem(item, today - datetime.timedelta(1))
 
     def handleItem(self, item, today):
         yesterday = today + datetime.timedelta(-1)
